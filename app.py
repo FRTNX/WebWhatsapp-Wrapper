0b7d67d6946f01ad032a5e701d0fb2355126cfe1
Ubuntu <ubuntu@ip-172-31-45-117.ec2.internal>
2019-03-26 16:26:49 +0000

    refactored

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 import os
+import sys
 import json
 import threading
 import requests
-from flask import Flask, request, Response
+from flask import Flask, request, Response, copy_current_request_context
 from webwhatsapi import WhatsAPIDriver
 
 application = Flask(__name__)
@@ -15,65 +16,66 @@ driver = None
 def get_numbers():
     global driver
     contacts_raw = request.args.get('contacts')
-    print('Recieved contacts: {}'.format(contacts_raw))
-    contacts = json.loads(contacts_raw)
-    print('input type: %s' % type(contacts))
+    print('Recieved contacts: {}'.format(contacts_raw), file=sys.stderr)
+    contacts = json.loads(contacts_raw.replace('"', ''))
+    print('input type: %s' % type(contacts), file=sys.stderr)
     if contacts:
         driver = WhatsAPIDriver(username="FRTNX", 
                                 client='Firefox', 
                                 headless=True)
         qr_string = driver.get_qr_plain()
+        @copy_current_request_context
+        def run(contacts):
+            # validate that contacts is of type list
+            print('waiting for connection', file=sys.stderr)
+            driver.wait_for_login()
+            print('getting chats', file=sys.stderr)
+            chats = driver.get_all_chats()
+            print(chats)
+            print('finding dialogue of interest', file=sys.stderr)
+            for contact in contacts:
+                print('Now searching for %s in in contacts' % contact, file=sys.stderr)
+                for c in chats:
+                    if str(contact) in str(c):
+                        chat = c
+                        print('dialogue search complete. found %s' % chat, file=sys.stderr)
+                        print('loading all earlier messages', file=sys.stderr)
+                        chat.load_all_earlier_messages()
+                        print('extracting all messages, self included', file=sys.stderr)
+                        messages = chat.get_messages(include_me=True)
+                        print('chats extraction successful', file=sys.stderr)
+                        processed = []
+                        print('beginning iteration through messages', file=sys.stderr)
+                        for msg in messages:
+                            try:
+                                js = msg.get_js_obj()
+                                content = js["content"]
+                                timestamp = js['timestamp']
+                                recipient = js['to']['user']
+                                if js['sender']['isMe']:
+                                    sender = '%s@Grassroot' % js['sender']['id']['user']
+                                else:
+                                    sender = js['chat']['contact']['name']
+                                processed.append({'content': content,
+                                                'timestamp': timestamp,
+                                                'sender': sender,
+                                                'recipient': recipient})
+                            except KeyError:
+                                f = open('errors.json', 'a')
+                                f.write(json.dumps(str(msg.get_js_obj()), indent=4))
+                                f.close()
+                                pass
+                        print('iteration complete. returning result', file=sys.stderr)
+                        print(processed, file=sys.stderr)
+                        # response = requests.post("https://bvjaygf3qd.execute-api.eu-west-1.amazonaws.com/dev/store", 
+                        #             data=json.dumps(processed))
+                        # print('response code: %s\nresponse content: %s' % (response.status_code, response.content))
+            shutdown_req = request.get("https://ii8i6r3mcd.execute-api.us-east-1.amazonaws.com/dev/stop_whatsapp_scraper")
+            print(shutdown_req.content, file=sys.stderr)
         threading.Thread(target=run, args=(contacts,)).start()
         return qr_string
     else:
         return 'No numbers sprecified'
-
-
-def run(contacts):
-    print('waiting for connection')
-    driver.wait_for_login()
-    print('getting chats')
-    chats = driver.get_all_chats()
-    print(chats)
-    print('finding dialogue of interest')
-    for contact in contacts:
-        for c in chats:
-            if contact in str(c):
-                chat = c
-                print('dialogue search complete. found %s' % chat)
-                print('loading all earlier messages')
-                chat.load_all_earlier_messages()
-                print('extracting all messages, self included')
-                messages = chat.get_messages(include_me=True)
-                print('chats extraction successful')
-                processed = []
-                print('beginning iteration through messages')
-                for msg in messages:
-                    try:
-                        js = msg.get_js_obj()
-                        content = js["content"]
-                        timestamp = js['timestamp']
-                        recipient = js['to']['user']
-                        if js['sender']['isMe']:
-                            sender = '%s@Grassroot' % js['sender']['id']['user']
-                        else:
-                            sender = js['chat']['contact']['name']
-                        processed.append({'content': content,
-                                        'timestamp': timestamp,
-                                        'sender': sender,
-                                        'recipient': recipient})
-                    except KeyError:
-                        f = open('errors.json', 'a')
-                        f.write(json.dumps(str(msg.get_js_obj()), indent=4))
-                        f.close()
-                        pass
-                print('iteration complete. returning result')
-                print(processed)
-                response = requests.post("https://bvjaygf3qd.execute-api.eu-west-1.amazonaws.com/dev/store", 
-                             data=json.dumps(processed))
-                print('response code: %s\nresponse content: %s' % (response.status_code, response.content))
-    shutdown_req = request.get("https://ii8i6r3mcd.execute-api.us-east-1.amazonaws.com/dev/stop_whatsapp_scraper")
-    print(shutdown_req.content)
 
 
 if __name__ == "__main__":
